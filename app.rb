114ee4c5fa9d755a93a0a5777b3ea51cd9a84489
Connor Worthington <worthington.connor@gmail.com>
2020-09-14 15:52:45 +0100

    add get name-cat

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ get '/secret' do
   "This is a secret. Shh!"
 end 
 
-get '/cat' do
-  @random_name = ["Amigo", "Oscar", "Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end 
+
+get '/named-cat' do 
+  p params
+  @name = params[:name]
   erb(:index)
 end 
