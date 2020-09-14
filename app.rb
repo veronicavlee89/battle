3eb335297257d75a394423e786dde41dce609b94
Veronica Lee <veronicavlee@icloud.com>
2020-09-14 15:40:35 +0100

    Add name string

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 
-#set :session_secret, 'secret session'
+set :session_secret, 'secret session'
+
+
 
 get '/' do
   "hello!"
@@ -11,5 +13,6 @@ get '/secret' do
 end 
 
 get '/cat' do
+  @random_name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end 
