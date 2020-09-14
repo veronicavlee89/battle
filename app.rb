bd2d41566b834af607dfd9a1dee9bb962afbafdb
Connor Worthington <worthington.connor@gmail.com>
2020-09-14 15:21:15 +0100

    adds get cat

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
 
+#set :session_secret, 'secret session'
+
 get '/' do
   "hello!"
 end
+
+get '/secret' do 
+  "This is a secret. Shh!"
+end 
+
+get '/cat' do 
+  '<img src="http://bit.ly/1eze8aE">'
+end 
