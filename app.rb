e92f5f4eb2dbe3d54ff388e9d6a9a9f117e0929e
Connor Worthington <worthington.connor@gmail.com>
2020-09-14 16:22:41 +0100

    adds cat_form

--- app.rb
+++ app.rb
@@ -17,8 +17,12 @@ get '/random-cat' do
   erb(:index)
 end 
 
-get '/named-cat' do 
+post '/named-cat' do 
   p params
   @name = params[:name]
   erb(:index)
+end 
+
+get '/cat-form' do 
+  erb(:cat_form)
 end 
